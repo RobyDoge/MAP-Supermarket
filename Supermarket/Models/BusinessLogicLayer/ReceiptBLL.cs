1d50df9e13572e796b52e00395b2403b8c0d40ba
RobyDoge <robyionut23@gmail.com>
2024-05-28 10:41:50 +0300

    Implemented UpdateStock

--- Supermarket/Models/BusinessLogicLayer/ReceiptBLL.cs
+++ Supermarket/Models/BusinessLogicLayer/ReceiptBLL.cs
@@ -8,6 +8,7 @@ public class ReceiptBLL(string cashierName)
     private string CashierName { get; set; } = cashierName;
     private ReceiptDAL ReceiptDal { get; set; } = new();
     private ProductDAL ProductDal { get; set; } = new();
+    private StockDAL StockDal { get; set; } = new();
 
     public void CreateReceipt(Dictionary<string, int> productList)
     {
@@ -17,7 +18,9 @@ public class ReceiptBLL(string cashierName)
         foreach (var product in productList)
         {
             productPrices.Add(product.Key, new Tuple<int, decimal>(product.Value, ProductDal.GetProductPrice(product.Key)));
-
+            //ai trebuie sa sterg si din stok cate produse am si daca ajung la 0 sa dezactivez stocul
+            //si daca e singurul stok sa dezactivez si produsul
+            StockDal.UpdateStock(product.Key, product.Value);
         }
         ShoppingListJson shoppingList = new(productPrices);
         ReceiptDal.CreateReceipt(CashierName, shoppingList.JsonList, shoppingList.TotalPrice);
--- Supermarket/Models/DataAccessLayer/StockDAL.cs
+++ Supermarket/Models/DataAccessLayer/StockDAL.cs
@@ -32,4 +32,71 @@ public class StockDAL
         var result = isAvailable.Value;
         return (bool)isAvailable.Value;
     }
+
+    public void UpdateStock(string productName, int quantity)
+    {
+        int stockId = DecreaseStock(productName, quantity);
+        DeleteEmptyStock(stockId);
+        DeactivateProductIfNoStockAvailable(productName);
+    }
+
+    private void DeactivateProductIfNoStockAvailable(string productName)
+    {
+        SqlConnection connection = DALHelper.Connection;
+        SqlCommand command = new("[DeactivateProductIfNoActiveStock]", connection)
+        {
+            CommandType = CommandType.StoredProcedure
+        };
+        SqlParameter productNameParam = new("@ProductName", SqlDbType.VarChar)
+        {
+            Value = productName
+        };
+        command.Parameters.Add(productNameParam);
+        connection.Open();
+        command.ExecuteNonQuery();
+        
+    }
+
+    private void DeleteEmptyStock(int stockId)
+    {
+        SqlConnection connection = DALHelper.Connection;
+        SqlCommand command = new("[DeactivateStockIfEmpty]", connection)
+        {
+            CommandType = CommandType.StoredProcedure
+        };
+        SqlParameter stockIdParam = new("@StockId", SqlDbType.Int)
+        {
+            Value = stockId
+        };
+        command.Parameters.Add(stockIdParam);
+        connection.Open();
+        command.ExecuteNonQuery();
+    }
+
+    private int DecreaseStock(string productName, int quantity)
+    {
+        SqlConnection connection = DALHelper.Connection;
+        SqlCommand command = new("[DecreaseStockByProductName]", connection)
+        {
+            CommandType = CommandType.StoredProcedure
+        };
+        SqlParameter productNameParam = new("@ProductName", SqlDbType.VarChar)
+        {
+            Value = productName
+        };
+        SqlParameter quantityParam = new("@Quantity", SqlDbType.Int)
+        {
+            Value = quantity
+        };
+        SqlParameter stockId = new("@StockId", SqlDbType.Int)
+        {
+            Direction = ParameterDirection.Output
+        };
+        command.Parameters.Add(productNameParam);
+        command.Parameters.Add(quantityParam);
+        command.Parameters.Add(stockId);
+        connection.Open();
+        command.ExecuteNonQuery();
+        return (int)stockId.Value;
+    }
 }
